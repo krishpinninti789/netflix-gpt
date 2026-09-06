6fd7c0ae63efba362809e325674a27e8f3133f7f
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-09-07 01:07:58 +0530

    used env to config

--- app/utils/fireBaseConfig.ts
+++ app/utils/fireBaseConfig.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBAM2Iz4HYKaqn92Oe1zBRTKAgLA5TfxeE",
-  authDomain: "netflix-gpt-965d1.firebaseapp.com",
-  projectId: "netflix-gpt-965d1",
-  storageBucket: "netflix-gpt-965d1.firebasestorage.app",
-  messagingSenderId: "181614411936",
-  appId: "1:181614411936:web:479abc0c4d5964bb3bc9c5",
-  measurementId: "G-HYH632KG89",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
